5040b084d7b159806cfaafc8e6b7d992f1ad27b1
mathiasbergqvist <mathiasbergqvist88@gmail.com>
2016-05-06 14:17:50 +0200

    adds flexbox layout and basic styling for initial components

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
+  View,
   Text,
-  View
+  StyleSheet,
+  StatusBarIOS
 } from 'react-native';
+
+StatusBarIOS.setStyle('light-content');
 
 class iTunesBrowser extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.mainContainer}>
+        <View style={styles.navbar}>
+          <View style={styles.navbarButton}></View>
+          <Text style={styles.navbarTitle}>
+            iTunesBrowser
+          </Text>
+          <Text style={styles.navbarButton}>
+            Search
+          </Text>
+        </View>
+        <View style={styles.content}>
+          <Text>
+            Bacon ipsum dolor amet ham hock brisket alcatra, shankle pork chop picanha hamburger. Tongue tail sausage bacon bresaola beef shoulder chicken venison shankle corned beef strip steak. T-bone short ribs sirloin tenderloin, beef tongue pork loin short loin beef ribs shank flank. Ball tip cupim sirloin boudin ribeye brisket flank. Chuck swine flank beef ribs, pastrami ham fatback strip steak landjaeger.
+          </Text>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  content: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#CCC'
   },
-  welcome: {
-    fontSize: 20,
+  navbar: {
+    backgroundColor: '#2a3744',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row'
+  },
+  navbarTitle: {
+    color: '#FEFEFE',
     textAlign: 'center',
-    margin: 10,
+    fontWeight: 'bold',
+    flex: 2
   },
-  instructions: {
+  navbarButton: {
+    width: 50,
+    color: '#FEFEFE',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('iTunesBrowser', () => iTunesBrowser);
