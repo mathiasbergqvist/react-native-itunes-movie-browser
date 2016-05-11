b48e49defbab705e31c80a250ef14ad86334c535
mathiasbergqvist <mathiasbergqvist88@gmail.com>
2016-05-11 13:00:37 +0200

    new component media cell, witch displays a single item including image and styling for the presentation

--- components/media-cell.js
+++ components/media-cell.js
@@ -21,6 +21,22 @@ var styles = StyleSheet.create({
     width: 60,
     marginRight: 8,
     resizeMode: 'contain'
+  },
+  cellTextContainer: {
+    flex: 1
+  },
+  mediaName: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 4
+  },
+  mediaDescription: {
+    fontSize: 12,
+    color: '#999',
+    flex: 1
+  },
+  mediaYear: {
+    fontWeight: 'bold'
   }
 });
 
@@ -37,6 +53,18 @@ var MediaCell = React.createClass({
               source={{uri: this.props.media.artworkUrl100}}
               style={styles.cellImage}
               />
+            <View style={styles.cellTextContainer}>
+              <Text style={styles.mediaName} numberOfLines={1}>
+                {this.props.media.trackName || this.props.media.collectionName}
+              </Text>
+              <Text style={styles.mediaDescription} numberOfLines={2}>
+                <Text style={styles.mediaYear}>
+                  {parseInt(this.props.media.releaseDate)}
+                </Text>
+                {" "}-{" "}
+                {this.props.media.longDescription || this.props.media.artistName}
+              </Text>
+            </View>
           </View>
         </TouchableHighlight>
       </View>
